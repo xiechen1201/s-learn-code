925607a4e8d0b9388461aeed05cd88b3a9ae95bf
xiechen@qihang <xiechen@kuaixie.com>
2026-06-02 15:37:49 +0800

    feat: update

---
+++ javascript-red-book-5/24/24.1.3/worker.js
@@ -0,0 +1,19 @@
+// ========= 案例一 =========
+/* self.onmessage = (e) => {
+  console.log("🚀 ~ worker.js ~ e:", e)
+} */
+
+// ========= 案例二 =========
+/* self.postMessage("foo");
+self.close()
+self.postMessage("bar");
+setTimeout(() => {
+  self.postMessage("baz");
+}, 0) */
+
+// ========= 案例三 =========
+/* self.onmessage = (e) => {
+  console.log("🚀 ~ worker.js ~ e:", e)
+} */
+
+// ========= 案例四 =========
